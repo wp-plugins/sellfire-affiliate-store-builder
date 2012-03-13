7f6a632967e9f78a333204335133d51b56f61d65
jmacinnes <jmacinnes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-13 14:47:40 +0000

    SellFire plugin initial commit
    git-svn-id: https://plugins.svn.wordpress.org/sellfire-affiliate-store-builder/trunk@518500 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,68 @@
+=== Plugin Name ===
+Contributors: jmacinnes
+Donate link: http://www.sellfire.com/
+Tags: affiliate, data feed, commission junction, cj, gan, amazon associates, 
+Requires at least: 3.0
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+SellFire's Store Builder makes it easy to earn money as an affiliate. Integrated with all of the biggest networks and thousands of merchants.
+
+== Description ==
+
+The SellFire Affiliate Store Builder Plugin gives affiliates an easy way to add product advertisements into their blog. You can use SellFire to search nearly 30 million product advertisements and create custom advertisement displays to add to your blog entry. All product links already have your affiliate IDs embedded into them.
+ 
+SellFire has integrated with the largest affiliate networks in the world. We have products from thousands of merchants. When you use SellFire, you do not need to download or import large data feed files or use a different tool for each affiliate network. You simply use SellFire.
+
+We currently work with:
+
+* Commission Junction
+* Link Share
+* Google Affiliate Network
+* PeperJam
+* ShareASale
+* Amazon
+
+== Frequently Asked Questions ==
+
+=What do I need to use the plug in?=
+To use the WordPress plug in, you will need:
+
+* A SellFire account
+* A web site running WordPress version 3.0 or higher
+* An affiliate account on one or more of our [network partners](http://www.sellfire.com/Networks "SellFire Affiliate Network Partners")
+
+=How much does the plug in cost=
+The WordPress plug in is free and licensed as open source software. If you are a developer, feel free to modify, extend and redistribute it. For the plugin to work, it must be linked to a SellFire account. You can sign up for SellFire for free at sellfire.com
+
+== Installation ==
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Unzip the zip archive to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add your SellFire Account API Key
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. Sample Fashion Blog with a SellFire store
+2. Sample Jewelery Blog with a SellFire store
+3. Screenshot of the plug in widget.
+4. SellFire Store Builder: Search screen - easily search and filter by multiple dimensions
+5. SellFire Store Builder: Edit screen - Customize the look and feel of your advertisements with our point and click editor
+
+== Changelog ==
+
+= 1.0 =
+* Plugin introduced
---
+++ sellfire.php
@@ -0,0 +1,234 @@
+<?php
+
+/*
+ * Plugin Name: SellFire Affiliate Store Builder
+ * Plugin URI: http://www.sellfiredev.com/Features/AffiliateWordPressPlugin
+ * Description: SellFire's store builder allows word press users to easily embed affiliate products into their blog. 
+ * Author: Jason MacInnes
+ * Author URI: http://www.jasonmacinnes.com
+ * License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+ */
+register_activation_hook(__FILE__, 'jem_sf_install');
+
+//adds the menu
+add_action('admin_menu', 'jem_sf_add_menus');
+
+//registers the settings
+add_action( 'admin_init', 'jem_sf_register_settings' );
+
+//registers the cache flush ajax handler
+add_action ( 'wp_ajax_jem_sf_flush_cache', 'jem_sf_flush_cache' );
+
+//strips out store tags
+//add_filter( 'the_content', 'jem_sf_replace_store_tags' );
+
+//register a shortcode
+add_shortcode( 'sellfire', 'jem_sf_sellfire_shortcode' );
+
+//SF Domain
+define ( 'JEM_SF_DOMAIN', 'http://www.sellfire.com' );
+
+//SF API Domain
+define ( 'JEM_SF_API_DOMAIN', 'https://www.sellfire.com' );
+
+//url of the SF API 
+define( 'JEM_SF_API_URL', JEM_SF_API_DOMAIN . '/Api/' );
+
+//define a constant to the JS directory
+define( 'JEM_SF_INSERTJS', plugin_dir_url(__FILE__).'js' );
+
+//define a constant to the CSS directory
+define( 'JEM_SF_INSERTCSS', plugin_dir_url(__FILE__).'css' );
+
+
+//include the definition of the post meta box
+include( 'includes/post-meta-box.php' );
+
+/*
+ * Installs the SellFire plug-in
+ */
+function jem_sf_install() {
+    $jem_sf_options = array(
+        'api_key' => ''
+    );
+    //todo: verify the existence of curl
+    update_option( 'jem_sf_options', $jem_sf_options );
+}
+
+/*
+ * Remove store tags that are embedded into a store's
+ * content
+ */
+function jem_sf_replace_store_tags( $content ) {
+    $match_found = 1;    
+    $match_pattern = '/<div[^<>]*?data-sf-storeid=["\']([\w]+)["\'][^<]*?[^<>]*?>[^<>]*?<\/div>/i';
+        
+    while (true)
+    {        
+        $match_found = preg_match( 
+                    $match_pattern, 
+                    $content, 
+                    $matches);
+        
+        if (! $match_found)
+        {            
+            break;
+        }
+        
+        $store_id = $matches[1];
+                
+        $store_content = jem_sf_get_url_contents( JEM_SF_DOMAIN . '/StoreDisplay/EmbeddedStore?logImpression=false&storeId=' . $store_id);
+        $content = preg_replace($match_pattern, jem_sf_preg_escape_back($store_content), $content);
+        return $content;
+    }
+    
+    return $content;
+}
+
+/*
+ * Replaces the sellfire shortcode with store contents
+ */
+function jem_sf_sellfire_shortcode($attr) {   
+    $store_content = get_transient(jem_sf_sellfire_transient_code($attr["id"]));
+    if (!$store_content || current_user_can('edit_posts'))
+    {
+        $response = wp_remote_get( JEM_SF_DOMAIN . '/StoreDisplay/EmbeddedStore?logImpression=false&storeId=' . $attr["id"]);
+        if (is_wp_error($response) || wp_remote_retrieve_response_code(&$response) != 200)
+        {
+            return '';
+        }
+        $store_content = wp_remote_retrieve_body(&$response);
+        set_transient(jem_sf_sellfire_transient_code($attr["id"]), $store_content);
+    }
+    return $store_content;
+}
+
+/*
+ * Given a store ID, returns the transient option code 
+ * for that store
+ */
+function jem_sf_sellfire_transient_code($store_id)
+{
+    return "sellfire-store-" . $store_id;
+}
+
+/*
+ * Removes characters that trigger a back-reference
+ */
+function jem_sf_preg_escape_back($string) { 
+    // Replace $ with \$ and \ with \\ 
+    $string = preg_replace('#(?<!\\\\)(\\$|\\\\)#', '\\\\$1', $string); 
+    return $string; 
+}
+
+/*
+ * Adds the appropriate menu's to the wordpress admin page
+ */
+function jem_sf_add_menus() {
+    add_options_page( 'SellFire Plugin Settings', 'SellFire', 'manage_options', __FILE__, 'jem_sf_settings' );        
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'jem_sf_jsscript', JEM_SF_INSERTJS . '/sellfire.js' );
+    $protocol = isset( $_SERVER["HTTPS"]) ? "https://" : "http://";
+    $params = array(
+        "ajaxurl" => admin_url('admin-ajax.php', $protocol));
+    
+    wp_localize_script('jem_sf_jsscript', 'jem_sf', $params);
+    wp_enqueue_style( 'jem_sf_cssscript', JEM_SF_INSERTCSS . '/sellfire.css' );
+}
+ 
+/*
+ * Register's SellFire settings with WP
+ */
+function jem_sf_register_settings() {
+    //validate setting updates
+    register_setting('jem_sf_options', 'jem_sf_options', 'jem_sf_validate_options');
+}
+
+/*
+ * Draws the settings page
+ */
+function jem_sf_settings() {
+    //add section for settings
+    add_settings_section('jem_sf_auth_section', 'Authentication Settings', 'jem_sf_auth_section_text', 'jem_sf_settings');
+
+    //add api key setting
+    add_settings_field('jem_sf_api_key', 'Your SellFire API key', 'jem_sf_api_key_input', 'jem_sf_settings', 'jem_sf_auth_section'); 
+
+    include('includes/options-page.php');
+}
+
+/*
+ * Description of the authentication section
+ */
+function jem_sf_auth_section_text() {
+    echo("To use the SellFire Plugin, you need to enter your API key. This can be found in your SellFire.com account page");
+}
+
+/*
+ * Draws the API key input box
+ */
+function jem_sf_api_key_input() {
+    $options = get_option( 'jem_sf_options' );
+    echo "<input id='api_key' style='width: 400px;' name='jem_sf_options[api_key]' type='text' value='{$options['api_key']}'/>";
+    echo "<div><a target='_SELLFIRE' href='https://www.sellfire.com/MyAccount/DataApiAccount'>Find my API Key</a></div>";
+}
+
+/*
+ * Validates the settings that have been posted
+ */
+function jem_sf_validate_options( $input ) {    
+    $api_key = $input['api_key'];
+    $url = JEM_SF_API_URL . 'ValidApiKey?apiKey=' . $api_key;
+    $params = array("sslverify" => false);
+    $response = wp_remote_get($url, $params);
+    $content = wp_remote_retrieve_body(&$response);
+    if ($content != "true")
+    {
+        add_settings_error('jem_sf_api_key', 'jem_sf_api_key_error', 'Invalid API key', 'error');
+    }
+    return $input;
+}
+
+/*
+ * Makes a call to the SellFire API for the specified operation
+ * and posts the JSON decoded values in post_values.
+ */
+function jem_sf_api_call( $api_operation, $post_values ) {
+    
+    if ($post_values == null)
+    {
+        $post_values = array();
+    }
+    
+    //create array of variables to post
+    $sf_options = get_option('jem_sf_options');
+
+    $post_values['apiKey'] = $sf_options['api_key'];
+    
+    $response = wp_remote_post( 
+            JEM_SF_API_URL . $api_operation, 
+            array ( 'body' => $post_values, "sslverify" => false) );
+
+    return json_decode( wp_remote_retrieve_body(&$response) );
+}
+
+/*
+ * Removes all of the store data from the cache
+ */
+function jem_sf_flush_cache ( ) {
+    
+    $response = jem_sf_api_call( 'ListStoreSummaries', null);
+    foreach ($response->Data as $store)
+    {
+        delete_transient(jem_sf_sellfire_transient_code($store->Id));
+    }
+}
+
+function jem_sf_get_ajax_url()
+{
+    $protocol = isset( $_SERVER["HTTPS"]) ? "https://" : "http://";
+    return admin_url('admin-ajax.php', $protocol);
+}
+
+?>
+
