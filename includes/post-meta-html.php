7d2dbd182690009210c9d1a9803f3de24c225a96
jmacinnes <jmacinnes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-15 18:11:51 +0000

    Fix header issue and create new release for bug fix
    git-svn-id: https://plugins.svn.wordpress.org/sellfire-affiliate-store-builder/tags/1.1@519468 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/post-meta-html.php
@@ -0,0 +1,34 @@
+<?php if ($response): ?>
+
+<table class="sf-store-meta-box">
+    <tr>                
+        <td>
+            Select Store
+        </td>
+        <td>
+            <select name="jem_sf_store" id="jem_sf_store">
+                <?php
+                    foreach ($response->Data as $store)
+                        echo "<option value='{$store->Id}'>{$store->Name}</option>";
+                ?>                
+            </select>
+            <input class="button-primary" type="submit" name="jem_sf_add_store" 
+                id="jem-sf-add-store" value="Add Store" onclick="jemSfAddShortCode(); return false;">
+            <div>
+                <a target="_SellFire" href="<?php echo JEM_SF_DOMAIN ?>/StoreBuilder/CreateUnnamedStore">
+                    Create new Store
+                </a>
+            </div>
+        </td>
+    </tr>
+</table>
+
+<?php else: ?>
+
+<div>
+    Enter your API key in the settings menu to use this plugin. If
+    you do not have an API key, you can get one for free by
+    registering at <a href="http://www.sellfire.com">SellFire.com</a>
+</div>
+
+<?php endif; ?>
