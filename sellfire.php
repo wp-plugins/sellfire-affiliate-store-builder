054833357fc97efb6812c99e0a452df779a62701
jmacinnes <jmacinnes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-02 18:38:00 +0000

    Fixing DEV url issue.
    git-svn-id: https://plugins.svn.wordpress.org/sellfire-affiliate-store-builder/trunk@647123 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sellfire.php
+++ sellfire.php
@@ -59,10 +59,10 @@ add_shortcode( 'sellFireQuick', 'jem_sf_sellfire_quick_shortcode' );
 add_shortcode( 'sellfirepp', 'jem_sf_sellfire_product_page_shortcode' );
 
 //SF Domain
-define ( 'JEM_SF_DOMAIN', 'http://www.sellfiredev.com:81' );
+define ( 'JEM_SF_DOMAIN', 'http://www.sellfire.com' );
 
 //SF API Domain
-define ( 'JEM_SF_API_DOMAIN', 'http://www.sellfiredev.com:81' );
+define ( 'JEM_SF_API_DOMAIN', 'https://www.sellfire.com' );
 
 //url of the SF API
 define( 'JEM_SF_API_URL', JEM_SF_API_DOMAIN . '/Api/' );
@@ -77,7 +77,7 @@ define( 'JEM_SF_INSERTJS', plugin_dir_url(__FILE__).'js' );
 define( 'JEM_SF_INSERTCSS', plugin_dir_url(__FILE__).'css' );
 
 //define a version number to communicate with the mother ship
-define('JEM_SF_VERSION', '2.5');
+define('JEM_SF_VERSION', '2.6');
 
 //include the definition of the post meta box
 include( 'includes/post-meta-box.php' );
